9f9da8cd55f55a07db4736cef5096e9c39420f9f
onesmus odongo <odongo1999@gmail.com>
2024-03-30 15:15:41 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+
+const victoryLab = "hi"
+
+//remove the node element by use of remove code 
+document.querySelector("main#main").remove();
+
+/* header assignment
+using your own names */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Onesmus Odongo is the champion!";
+newHeader.className = "victory";
